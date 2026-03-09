8d575b31366d840d549245eb03f10f6cec3a9e09
edison9733 <eliu4864@gmail.com>
2026-03-09 08:50:17 +0800

    bg v3

--- public/components/DotBackground.js
+++ public/components/DotBackground.js
@@ -7,6 +7,7 @@ export default function DotBackground() {
       backgroundImage: 'url(/bg-texture.png)',
       backgroundRepeat: 'repeat',
       backgroundSize: 'auto',
+      opacity: 1.0,
     }} />
   );
 }
